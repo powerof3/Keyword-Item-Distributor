fc1fba5c1f4af87add833505a591b26a2361a010
powerof3 <powerof3@users.noreply.github.com>
2026-06-25 09:06:41 +0530

    Revert to using `std::optional` for `Traits`

--- include/Data/Traits.h
+++ include/Data/Traits.h
@@ -14,13 +14,13 @@ public:
 
 protected:
 	template <class T>
-	static T extract_single_value(const std::string& a_str)
+	static std::optional<T> extract_single_value(const std::string& a_str)
 	{
 		static const srell::regex re{ R"([-+]?\d*\.?\d+)" };
 		if (srell::smatch m; srell::regex_search(a_str, m, re)) {
 			return string::to_num<T>(m[0].str());
 		}
-		return static_cast<T>(-1);
+		return std::nullopt;
 	}
 };
 
@@ -32,9 +32,13 @@ public:
 		auto traits = distribution::split_entry(a_traits);
 		for (const auto& trait : traits) {
 			if (trait.contains("AR(")) {
-				armorRating = Range<float>(trait);
+				if (auto range = Range<float>(trait); range.IsValid()) {
+					armorRating = range;
+				}
 			} else if (trait.contains("W(")) {
-				weight = Range<float>(trait);
+				if (auto range = Range<float>(trait); range.IsValid()) {
+					weight = range;
+				}
 			} else if (string::is_only_digit(trait)) {
 				slot = static_cast<RE::BIPED_MODEL::BipedObjectSlot>(1 << (string::to_num<std::uint32_t>(trait) - 30));
 			} else {
@@ -78,16 +82,16 @@ public:
 		if (templated && (armor->templateArmor != nullptr) != *templated) {
 			return false;
 		}
-		if (armorType != static_cast<RE::BIPED_MODEL::ArmorType>(-1) && armor->GetArmorType() != armorType) {
+		if (armorType && armor->GetArmorType() != *armorType) {
 			return false;
 		}
-		if (slot != RE::BIPED_MODEL::BipedObjectSlot::kNone && !armor->HasPartOf(slot)) {
+		if (slot && !armor->HasPartOf(*slot)) {
 			return false;
 		}
-		if (weight.IsValid() && !weight.IsInRange(armor->weight)) {
+		if (weight && !weight->IsInRange(armor->weight)) {
 			return false;
 		}
-		if (armorRating.IsValid() && !armorRating.IsInRange(armor->GetArmorRating())) {
+		if (armorRating && !armorRating->IsInRange(armor->GetArmorRating())) {
 			return false;
 		}
 		return true;
@@ -95,12 +99,12 @@ public:
 
 private:
 	// members
-	std::optional<bool>              enchanted{};
-	std::optional<bool>              templated{};
-	Range<float>                     armorRating{};
-	RE::BIPED_MODEL::ArmorType       armorType{ static_cast<RE::BIPED_MODEL::ArmorType>(-1) };
-	Range<float>                     weight;
-	RE::BIPED_MODEL::BipedObjectSlot slot{ RE::BIPED_MODEL::BipedObjectSlot::kNone };
+	std::optional<bool>                             enchanted{};
+	std::optional<bool>                             templated{};
+	std::optional<Range<float>>                     armorRating{};
+	std::optional<RE::BIPED_MODEL::ArmorType>       armorType{};
+	std::optional<Range<float>>                     weight{};
+	std::optional<RE::BIPED_MODEL::BipedObjectSlot> slot{};
 };
 
 class WeaponTraits : public Traits
@@ -111,9 +115,13 @@ public:
 		auto traits = distribution::split_entry(a_traits);
 		for (const auto& trait : traits) {
 			if (trait.contains("W(")) {
-				weight = Range<float>(trait);
+				if (auto range = Range<float>(trait); range.IsValid()) {
+					weight = range;
+				}
 			} else if (trait.contains("D(")) {
-				damage = Range<float>(trait);
+				if (auto range = Range<float>(trait); range.IsValid()) {
+					damage = range;
+				}
 			} else {
 				switch (string::const_hash(trait)) {
 				case "HandToHandMelee"_h:
@@ -176,13 +184,13 @@ public:
 		if (templated && (weapon->templateWeapon != nullptr) != *templated) {
 			return false;
 		}
-		if (animationType != static_cast<RE::WEAPON_TYPE>(-1) && weapon->GetWeaponType() != animationType) {
+		if (animationType && weapon->GetWeaponType() != *animationType) {
 			return false;
 		}
-		if (weight.IsValid() && !weight.IsInRange(weapon->weight)) {
+		if (weight && !weight->IsInRange(weapon->weight)) {
 			return false;
 		}
-		if (damage.IsValid() && !damage.IsInRange(weapon->GetAttackDamage())) {
+		if (damage && !damage->IsInRange(weapon->GetAttackDamage())) {
 			return false;
 		}
 		return true;
@@ -190,11 +198,11 @@ public:
 
 private:
 	// members
-	std::optional<bool> enchanted{};
-	std::optional<bool> templated{};
-	RE::WEAPON_TYPE     animationType{ static_cast<RE::WEAPON_TYPE>(-1) };
-	Range<float>        damage{};
-	Range<float>        weight{};
+	std::optional<bool>            enchanted{};
+	std::optional<bool>            templated{};
+	std::optional<RE::WEAPON_TYPE> animationType{};
+	std::optional<Range<float>>    damage{};
+	std::optional<Range<float>>    weight{};
 };
 
 class AmmoTraits : public Traits
@@ -205,7 +213,9 @@ public:
 		auto traits = distribution::split_entry(a_traits);
 		for (const auto& trait : traits) {
 			if (trait.contains("D(")) {
-				damage = Range<float>(trait);
+				if (auto range = Range<float>(trait); range.IsValid()) {
+					damage = range;
+				}
 			} else {
 				switch (string::const_hash(trait)) {
 				case "B"_h:
@@ -229,7 +239,7 @@ public:
 		if (isBolt && ammo->IsBolt() != *isBolt) {
 			return false;
 		}
-		if (damage.IsValid() && !damage.IsInRange(ammo->data.damage)) {
+		if (damage && !damage->IsInRange(ammo->data.damage)) {
 			return false;
 		}
 		return true;
@@ -237,8 +247,8 @@ public:
 
 private:
 	// members
-	Range<float>        damage;
-	std::optional<bool> isBolt;
+	std::optional<Range<float>> damage{};
+	std::optional<bool>         isBolt{};
 };
 
 class MagicEffectTraits : public Traits
@@ -271,9 +281,9 @@ public:
 					auto min = string::to_num<std::int32_t>(m[2].str());
 					if (m[3].matched) {
 						auto max = string::to_num<std::int32_t>(m[3].str());
-						skill = { skillType, { min, max } };
+						skill = std::make_pair(skillType, Range<std::int32_t>{ min, max });
 					} else {
-						skill = { skillType, { min } };
+						skill = std::make_pair(skillType, Range<std::int32_t>{ min });
 					}
 				}
 			}
@@ -285,13 +295,13 @@ public:
 	{
 		const auto mgef = a_item->As<RE::EffectSetting>();
 
-		if (castingType != static_cast<RE::MagicSystem::CastingType>(-1) && mgef->data.castingType != castingType) {
+		if (castingType && mgef->data.castingType != *castingType) {
 			return false;
 		}
-		if (deliveryType != static_cast<RE::MagicSystem::Delivery>(-1) && mgef->data.delivery != deliveryType) {
+		if (deliveryType && mgef->data.delivery != *deliveryType) {
 			return false;
 		}
-		if (resistance != RE::ActorValue::kNone && mgef->data.resistVariable != resistance) {
+		if (resistance && mgef->data.resistVariable != *resistance) {
 			return false;
 		}
 		if (dispelWithKeywords && mgef->data.flags.all(RE::EffectSetting::EffectSettingData::Flag::kDispelWithKeywords) != *dispelWithKeywords) {
@@ -300,9 +310,9 @@ public:
 		if (isHostile && mgef->IsHostile() != *isHostile) {
 			return false;
 		}
-		if (skill.second.IsValid()) {
-			auto& [skillType, minMax] = skill;
-			if (skillType != RE::ActorValue::kNone && skillType != mgef->GetMagickSkill()) {
+		if (skill) {
+			const auto& [skillType, minMax] = *skill;
+			if (skillType != mgef->GetMagickSkill()) {
 				return false;
 			}
 			if (!minMax.IsInRange(mgef->GetMinimumSkillLevel())) {
@@ -314,12 +324,12 @@ public:
 
 private:
 	// members
-	RE::MagicSystem::CastingType                   castingType{ static_cast<RE::MagicSystem::CastingType>(-1) };
-	RE::MagicSystem::Delivery                      deliveryType{ static_cast<RE::MagicSystem::Delivery>(-1) };
-	std::pair<RE::ActorValue, Range<std::int32_t>> skill{ RE::ActorValue::kNone, {} };
-	RE::ActorValue                                 resistance{ RE::ActorValue::kNone };
-	std::optional<bool>                            isHostile;
-	std::optional<bool>                            dispelWithKeywords;
+	std::optional<RE::MagicSystem::CastingType>                   castingType{};
+	std::optional<RE::MagicSystem::Delivery>                      deliveryType{};
+	std::optional<std::pair<RE::ActorValue, Range<std::int32_t>>> skill{};
+	std::optional<RE::ActorValue>                                 resistance{};
+	std::optional<bool>                                           isHostile{};
+	std::optional<bool>                                           dispelWithKeywords{};
 };
 
 class PotionTraits : public Traits
@@ -364,8 +374,8 @@ public:
 
 private:
 	// members
-	std::optional<bool> isPoison;
-	std::optional<bool> isFood;
+	std::optional<bool> isPoison{};
+	std::optional<bool> isFood{};
 };
 
 class IngredientTraits : public Traits
@@ -396,7 +406,7 @@ public:
 
 private:
 	// members
-	std::optional<bool> isFood;
+	std::optional<bool> isFood{};
 };
 
 class BookTraits : public Traits
@@ -439,9 +449,9 @@ public:
 		if (teachesSkill && book->TeachesSkill() != *teachesSkill) {
 			return false;
 		}
-		if (actorValue != RE::ActorValue::kNone) {
+		if (actorValue) {
 			const auto taughtSpell = book->GetSpell();
-			if (book->GetSkill() != actorValue && (taughtSpell && AV::GetAssociatedSkill(taughtSpell) != actorValue)) {
+			if (book->GetSkill() != *actorValue && (taughtSpell && AV::GetAssociatedSkill(taughtSpell) != *actorValue)) {
 				return false;
 			}
 		}
@@ -450,9 +460,9 @@ public:
 
 private:
 	// members
-	std::optional<bool> teachesSpell;
-	std::optional<bool> teachesSkill;
-	RE::ActorValue      actorValue{ RE::ActorValue::kNone };
+	std::optional<bool>           teachesSpell{};
+	std::optional<bool>           teachesSkill{};
+	std::optional<RE::ActorValue> actorValue{};
 };
 
 class SoulGemTraits : public Traits
@@ -482,10 +492,10 @@ public:
 		if (black && soulGem->CanHoldNPCSoul() != *black) {
 			return false;
 		}
-		if (soulSize != static_cast<RE::SOUL_LEVEL>(-1) && soulGem->GetContainedSoul() != soulSize) {
+		if (soulSize && soulGem->GetContainedSoul() != *soulSize) {
 			return false;
 		}
-		if (gemSize != static_cast<RE::SOUL_LEVEL>(-1) && soulGem->GetMaximumCapacity() != gemSize) {
+		if (gemSize && soulGem->GetMaximumCapacity() != *gemSize) {
 			return false;
 		}
 
@@ -494,9 +504,9 @@ public:
 
 private:
 	// members
-	std::optional<bool> black;
-	RE::SOUL_LEVEL      soulSize{ static_cast<RE::SOUL_LEVEL>(-1) };
-	RE::SOUL_LEVEL      gemSize{ static_cast<RE::SOUL_LEVEL>(-1) };
+	std::optional<bool>           black{};
+	std::optional<RE::SOUL_LEVEL> soulSize{};
+	std::optional<RE::SOUL_LEVEL> gemSize{};
 };
 
 class SpellTraits : public Traits
@@ -527,19 +537,19 @@ public:
 	{
 		const auto spell = a_item->As<RE::MagicItem>();
 
-		if (spellType != static_cast<RE::MagicSystem::SpellType>(-1) && spell->GetSpellType() != spellType) {
+		if (spellType && spell->GetSpellType() != *spellType) {
 			return false;
 		}
-		if (castingType != static_cast<RE::MagicSystem::CastingType>(-1) && spell->GetCastingType() != castingType) {
+		if (castingType && spell->GetCastingType() != *castingType) {
 			return false;
 		}
-		if (deliveryType != static_cast<RE::MagicSystem::Delivery>(-1) && spell->GetDelivery() != deliveryType) {
+		if (deliveryType && spell->GetDelivery() != *deliveryType) {
 			return false;
 		}
 		if (isHostile && spell->IsHostile() != *isHostile) {
 			return false;
 		}
-		if (skill != RE::ActorValue::kNone && AV::GetAssociatedSkill(spell) != skill) {
+		if (skill && AV::GetAssociatedSkill(spell) != *skill) {
 			return false;
 		}
 
@@ -548,11 +558,11 @@ public:
 
 private:
 	// members
-	RE::MagicSystem::SpellType   spellType{ static_cast<RE::MagicSystem::SpellType>(-1) };
-	RE::MagicSystem::CastingType castingType{ static_cast<RE::MagicSystem::CastingType>(-1) };
-	RE::MagicSystem::Delivery    deliveryType{ static_cast<RE::MagicSystem::Delivery>(-1) };
-	RE::ActorValue               skill{ RE::ActorValue::kNone };
-	std::optional<bool>          isHostile{};
+	std::optional<RE::MagicSystem::SpellType>   spellType{};
+	std::optional<RE::MagicSystem::CastingType> castingType{};
+	std::optional<RE::MagicSystem::Delivery>    deliveryType{};
+	std::optional<RE::ActorValue>               skill{};
+	std::optional<bool>                         isHostile{};
 };
 
 class FurnitureTraits : public Traits
@@ -577,13 +587,13 @@ public:
 	{
 		const auto furniture = a_item->As<RE::TESFurniture>();
 
-		if (benchType != static_cast<RE::TESFurniture::WorkBenchData::BenchType>(-1) && furniture->workBenchData.benchType != benchType) {
+		if (benchType && furniture->workBenchData.benchType != *benchType) {
 			return false;
 		}
-		if (useSkill != RE::ActorValue::kNone && furniture->workBenchData.usesSkill != useSkill) {
+		if (useSkill && furniture->workBenchData.usesSkill != *useSkill) {
 			return false;
 		}
-		if (furnitureType != static_cast<std::int32_t>(-1) && GetFurnitureType(furniture) != furnitureType) {
+		if (furnitureType && GetFurnitureType(furniture) != *furnitureType) {
 			return false;
 		}
 
@@ -612,9 +622,9 @@ private:
 	}
 
 	// members
-	std::int32_t                               furnitureType{ static_cast<std::int32_t>(-1) };
-	RE::TESFurniture::WorkBenchData::BenchType benchType{ static_cast<RE::TESFurniture::WorkBenchData::BenchType>(-1) };
-	RE::ActorValue                             useSkill{ RE::ActorValue::kNone };
+	std::optional<std::int32_t>                               furnitureType{};
+	std::optional<RE::TESFurniture::WorkBenchData::BenchType> benchType{};
+	std::optional<RE::ActorValue>                             useSkill{};
 };
 
 inline std::shared_ptr<Traits> Traits::Create(DISTRIBUTION::TYPE a_type, const std::string& a_traits)
